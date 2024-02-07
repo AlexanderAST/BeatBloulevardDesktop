2b794d215038e3d0a7d5a13746475ab002f69896
Alexander Astakhov <89164909109@bk.ru>
2024-02-07 21:52:25 +0300

    Добавьте файлы проекта.

---
+++ BeatBloulevardDesktop/AuthPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Services.Description;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+
+namespace BeatBloulevardDesktop
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+           
+
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new HttpClient();
+            var postData = new
+            {
+                email = _login.Text,
+                password = _password.Password
+            };
+
+            var json = JsonConvert.SerializeObject(postData);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("http://localhost:8081/sign-in", content);
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("Вы авторизованы");
+                NavigationService.Navigate(new MainPage());
+                    
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль");
+            }
+        }
+    }
+}
